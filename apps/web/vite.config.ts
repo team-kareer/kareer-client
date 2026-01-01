5cd91afec9e30877da67aaf33ccfb1f1eb1408b1
jeonghoon11 <jih361@naver.com>
2026-01-02 01:34:15 +0900

    feat: 절대경로 가이드라인 추가

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
@@ -10,4 +11,12 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      // 절대경로 세팅 위치
+      '@app': path.resolve(__dirname, 'src/app'),
+      // '@pages': path.resolve(__dirname, 'src/pages'),
+      // 등등. 등.
+    },
+  },
 });
