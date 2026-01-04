1b83fec2617b15f33906134d3dfbc6904c6438f9
huniversal <hunivers0523@naver.com>
2026-01-04 13:04:58 +0900

    refactor: react-router로 import 경로 변경

--- apps/web/src/app/App.tsx
+++ apps/web/src/app/App.tsx
@@ -1,4 +1,4 @@
-import { RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router';
 import { router } from '@shared/router';
 
 function App() {
--- apps/web/src/shared/router/router.tsx
+++ apps/web/src/shared/router/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router';
 import { globalRoutes } from '@shared/router/routes/global-routes';
 import GlobalLayout from '@shared/router/global-layout';
 
