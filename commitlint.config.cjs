666bf85ca997e7cf93e2447328ec53c022e52838
yooncandooit <dotsi0@dgu.ac.kr>
2026-01-06 01:31:17 +0900

    chore: 허용 커밋 타입 추가

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -4,31 +4,26 @@ const commitMsgFilePath = process.argv[2];
 const commitMessage = fs.readFileSync(commitMsgFilePath, 'utf8').trim();
 
 const allowedTypes = [
-  'feat', // 새로운 기능
-  'fix', // 버그 수정
-  'refactor', // 리팩토링
-  'design', // CSS 등 UI 디자인 변경
-  'style', // 코드 포맷팅
-  'docs', // 문서 수정
-  'test', // 테스트 코드
-  'chore', // 빌드, 패키지 등
-  'init', // 프로젝트 초기화
-  'rename', // 파일/폴더명 변경
-  'remove', // 파일 삭제
+  'feat',
+  'fix',
+  'refactor',
+  'docs',
+  'init',
+  'style',
+  'chore',
+  'test',
+  'deploy',
+  'merge',
+  'Merge',
 ];
 
-// "type: message" 형식 체크
 const commitRegex = /^(\w+):\s{1,}(.+)$/;
 const match = commitMessage.match(commitRegex);
 
 if (!match) {
   console.error(`
 ❌ 커밋 실패!
-❗ 형식: "type: message" (type과 message 사이 공백 필수)
-   
-📌 예시:
-   feat: 로그인 기능 추가
-   fix: 대시보드 버그 수정
+❗ 형식은 반드시 "type: message" 형태여야 하며, type과 message 사이엔 공백이 1개 이상 있어야 합니다.
 `);
   process.exit(1);
 }
@@ -39,10 +34,8 @@ if (!allowedTypes.includes(type.toLowerCase())) {
   console.error(`
 ❌ 커밋 실패!
 ❗ "${type}"는 허용되지 않은 커밋 타입입니다.
-
-✅ 허용된 타입: ${allowedTypes.join(', ')}
 `);
   process.exit(1);
 }
 
-console.log('✅ 커밋 메시지 검증 통과!');
+console.log('커밋 성공!');
