18db3031c44645f9eca26a3ab7b1305dd0e2b975
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-01-06 05:30:05 +0900

    feat: vanilla-extract plugin 설정

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,3 +1,4 @@
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
@@ -10,6 +11,7 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    vanillaExtractPlugin(),
   ],
   resolve: {
     alias: {
