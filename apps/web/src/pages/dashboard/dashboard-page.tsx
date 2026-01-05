31967d56720fd4a797e5a3cd3cf2ca0432d00040
huniversal <hunivers0523@naver.com>
2026-01-05 14:09:41 +0900

    refactor: 테스트 코드 제거

--- apps/web/src/pages/dashboard/dashboard-page.tsx
+++ apps/web/src/pages/dashboard/dashboard-page.tsx
@@ -1,20 +1,6 @@
-// const DashboardPage = () => {
-//   return <div>DashboardPage</div>;
-// };
-
-// export default DashboardPage;
-
-// 테스트 코드 (PR 리뷰 후 삭제 예정)
-import { useEffect } from 'react';
-
-import { testUser } from '@shared/apis/test';
-
 const DashboardPage = () => {
-  useEffect(() => {
-    testUser();
-  }, []);
-
   return <div>DashboardPage</div>;
 };
 
 export default DashboardPage;
+
