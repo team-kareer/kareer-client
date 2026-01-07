7c043df9d63ac5b7f3b075aa08c84b383ab761c9
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-01-08 00:36:50 +0900

    refactor: @ts-expect-error 타입 선언으로 대체

--- apps/web/src/shared/apis/providers/devtools-provider.tsx
+++ apps/web/src/shared/apis/providers/devtools-provider.tsx
@@ -11,8 +11,6 @@ const DevtoolsProvider = () => {
   const [showDevtools, setShowDevtools] = useState(false);
 
   useEffect(() => {
-    // window 객체에 함수 등록
-    // @ts-expect-error : window에 원래 있는 속성이 아니라서 ts 에러가 나니 무시
     window.toggleDevtools = () => setShowDevtools((prev) => !prev);
   }, []);
 
