22134de7f474708a158ef9ee5324be760d0c7b3d
huniversal <hunivers0523@naver.com>
2026-01-09 15:56:03 +0900

    test: Tag 컴포넌트 테스트

--- apps/web/src/pages/dashboard/dashboard-page.tsx
+++ apps/web/src/pages/dashboard/dashboard-page.tsx
@@ -1,5 +1,44 @@
+import { Tag } from '@kds/ui';
+
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <div
+      style={{
+        padding: '2rem',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1rem',
+      }}
+    >
+      <h1>Tag 컴포넌트 테스트</h1>
+
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '0.8rem' }}>
+        <Tag color="primary_blue">text</Tag>
+        <Tag color="primary_black">text</Tag>
+      </div>
+
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '0.8rem' }}>
+        <Tag color="pastel_blue">text</Tag>
+        <Tag color="pastel_red">text</Tag>
+        <Tag color="pastel_mint">text</Tag>
+        <Tag color="pastel_orange">text</Tag>
+        <Tag color="pastel_pink">text</Tag>
+        <Tag color="pastel_purple">text</Tag>
+        <Tag color="pastel_skyblue">text</Tag>
+      </div>
+
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '0.8rem' }}>
+        <Tag color="outlined_blue">text</Tag>
+        <Tag color="outlined_black">text</Tag>
+      </div>
+
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '0.8rem' }}>
+        <Tag color="primary_blue" disabled>
+          text
+        </Tag>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
