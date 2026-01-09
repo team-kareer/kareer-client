f249ac76e5ca0a89e3027fc68c01f68f9e445bf9
huniversal <hunivers0523@naver.com>
2026-01-09 15:55:22 +0900

    feat: Tag 컴포넌트 구현

--- packages/kds-ui/src/components/tag/tag.tsx
+++ packages/kds-ui/src/components/tag/tag.tsx
@@ -1,5 +1,18 @@
-const Tag = () => {
-  return <div>Tag</div>;
+import { ButtonHTMLAttributes, ReactNode } from 'react';
+
+import { tagColor } from './tag.css';
+
+interface TagProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  color: keyof typeof tagColor;
+  children: ReactNode;
+}
+
+const Tag = ({ color, disabled = false, children, ...props }: TagProps) => {
+  return (
+    <button className={tagColor[color]} disabled={disabled} {...props}>
+      {children}
+    </button>
+  );
 };
 
 export default Tag;
