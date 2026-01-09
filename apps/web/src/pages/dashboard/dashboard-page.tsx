638102a11cf838d634f082f7d42b615461ed971b
huniversal <hunivers0523@naver.com>
2026-01-10 02:52:12 +0900

    fix: test 코드 제거

--- apps/web/src/pages/dashboard/dashboard-page.tsx
+++ apps/web/src/pages/dashboard/dashboard-page.tsx
@@ -1,43 +1,5 @@
-import { Tag } from '@kds/ui';
-
 const DashboardPage = () => {
-  return (
-    <div
-      style={{
-        padding: '2rem',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '1rem',
-      }}
-    >
-      <h1>Tag 컴포넌트 테스트</h1>
-
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '0.8rem' }}>
-        <Tag color="primary_blue">text</Tag>
-        <Tag color="primary_black">text</Tag>
-      </div>
-
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '0.8rem' }}>
-        <Tag color="pastel_blue">text</Tag>
-        <Tag color="pastel_red">text</Tag>
-        <Tag color="pastel_mint">text</Tag>
-        <Tag color="pastel_orange">text</Tag>
-        <Tag color="pastel_pink">text</Tag>
-        <Tag color="pastel_purple">text</Tag>
-        <Tag color="pastel_skyblue">text</Tag>
-      </div>
-
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '0.8rem' }}>
-        <Tag color="outlined_blue">text</Tag>
-        <Tag color="outlined_black">text</Tag>
-        <Tag color="disabled_gray">text</Tag>
-      </div>
-
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '0.8rem' }}>
-        <Tag color="navigation">home</Tag>
-      </div>
-    </div>
-  );
+  return <div>DashboardPage</div>;
 };
 
 export default DashboardPage;
