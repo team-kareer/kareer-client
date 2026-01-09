1b37a7a72eec3b5daaae8fb2cb9268b536682abd
jeonghoon11 <jih361@naver.com>
2026-01-09 15:56:59 +0900

    feat: TodoPanel 컴포넌트 및 layout 작업

---
+++ apps/web/src/widgets/layout/components/todo-panel/todo-panel.css.ts
@@ -0,0 +1,9 @@
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  width: '25.6rem',
+  padding: '2rem',
+  marginRight: '2rem',
+});
---
+++ apps/web/src/widgets/layout/components/todo-panel/todo-panel.tsx
@@ -0,0 +1,7 @@
+import * as styles from './todo-panel.css';
+
+const TodoPanel = () => {
+  return <div className={styles.container}>TodoPanel</div>;
+};
+
+export default TodoPanel;
