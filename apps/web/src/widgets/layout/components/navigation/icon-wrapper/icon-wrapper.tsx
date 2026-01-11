a95ef4c164553157ee1914070e36d650cf1fda4a
jeonghoon11 <jih361@naver.com>
2026-01-11 23:46:00 +0900

    feat: iconWrapper에 Tag 추가

--- apps/web/src/widgets/layout/components/navigation/icon-wrapper/icon-wrapper.tsx
+++ apps/web/src/widgets/layout/components/navigation/icon-wrapper/icon-wrapper.tsx
@@ -1,16 +1,23 @@
 import { HTMLAttributes, ReactNode } from 'react';
+import { Tag } from '@kds/ui';
 
 import * as styles from './icon-wrapper.css';
 
 interface IconWrapperProps extends HTMLAttributes<HTMLButtonElement> {
   icon: ReactNode;
+  label?: string;
   isActive?: boolean;
 }
 
-const IconWrapper = ({ icon, isActive, ...props }: IconWrapperProps) => {
+const IconWrapper = ({ icon, label, isActive, ...props }: IconWrapperProps) => {
   return (
     <button className={styles.wrapper({ isActive })} {...props}>
       <span className={styles.icon}>{icon}</span>
+      {label && (
+        <span className={styles.tag}>
+          <Tag color="navigation">{label}</Tag>
+        </span>
+      )}
     </button>
   );
 };
