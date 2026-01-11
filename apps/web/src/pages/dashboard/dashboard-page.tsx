c39f44b2b1d6c8be5b78376e07d5b9ca5df1dcbe
huniversal <hunivers0523@naver.com>
2026-01-12 02:00:08 +0900

    feat: merge 전 테스트 코드 삭제

--- apps/web/src/pages/dashboard/dashboard-page.tsx
+++ apps/web/src/pages/dashboard/dashboard-page.tsx
@@ -1,38 +1,5 @@
-import { ActionCard } from '@widgets/roadmap/components/action-card';
-
 const DashboardPage = () => {
-  const action = {
-    title: 'Action Required',
-    subTitle: 'This is a sub title',
-    dueDate: '2026-01-01',
-    disabled: true,
-  };
-
-  return (
-    <div
-      style={{
-        display: 'flex',
-        width: '455px',
-        backgroundColor: 'lightgray',
-        flexDirection: 'column',
-        padding: '2rem',
-        gap: '1rem',
-      }}
-    >
-      <ActionCard
-        title={action.title}
-        subTitle={action.subTitle}
-        dueDate={action.dueDate}
-        disabled={true}
-      />
-      <ActionCard
-        title={action.title}
-        subTitle={action.subTitle}
-        dueDate={action.dueDate}
-        disabled={false}
-      />
-    </div>
-  );
+  return <div>Dashboard Page</div>;
 };
 
 export default DashboardPage;
