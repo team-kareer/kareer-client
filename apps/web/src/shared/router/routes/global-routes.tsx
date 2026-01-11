056e8cf4154e935fc6cef08d3c9e0139dff24795
jeonghoon11 <jih361@naver.com>
2026-01-12 01:24:06 +0900

    feat: global-routes 파일에서 FitAnalysisPage 임포트 방식 수정

--- apps/web/src/shared/router/routes/global-routes.tsx
+++ apps/web/src/shared/router/routes/global-routes.tsx
@@ -1,5 +1,9 @@
-import FitAnalysisPage from '@pages/fit-analysis/fit-analysis-page';
-import { DashboardPage, MyPage, RoadmapPage } from '@shared/router/lazy';
+import {
+  DashboardPage,
+  FitAnalysisPage,
+  MyPage,
+  RoadmapPage,
+} from '@shared/router/lazy';
 import { ROUTE_PATH } from '@shared/router/path';
 
 export const globalRoutes = [
