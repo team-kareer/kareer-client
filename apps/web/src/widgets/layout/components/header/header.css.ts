f8ed8333c7d46416e70d29633e47b83ef21f8527
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-01-12 21:50:27 +0900

    feat: header 컴포넌트 구현

--- apps/web/src/widgets/layout/components/header/header.css.ts
+++ apps/web/src/widgets/layout/components/header/header.css.ts
@@ -1,9 +1,32 @@
-import { themeVars } from '@kds/ui/styles';
+import { themeVars, typography } from '@kds/ui/styles';
 import { style } from '@vanilla-extract/css';
 
 export const container = style({
   display: 'flex',
+  justifyContent: 'space-between',
   padding: '2rem',
   backgroundColor: themeVars.color.grayscale.white,
   borderBottom: `1px solid ${themeVars.color.grayscale.gray300}`,
+});
+
+export const left_section = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.4rem',
+});
+
+export const title = style({
+  ...typography.sub1_sb_22,
+  color: themeVars.color.grayscale.gray800,
+});
+
+export const subTitle = style({
+  ...typography.body6_r_16,
+  color: themeVars.color.grayscale.gray500,
+});
+
+export const img = style({
+  width: '5.2rem',
+  height: '5.2rem',
+  borderRadius: '50%',
 });
--- apps/web/src/widgets/layout/components/header/header.tsx
+++ apps/web/src/widgets/layout/components/header/header.tsx
@@ -1,7 +1,71 @@
+import { UserProfileIcon } from '@kds/icons';
+import { useLocation } from 'react-router';
+
+import { ROUTE_PATH } from '@shared/router';
+
 import * as styles from './header.css';
 
+const TITLE_LIST = [
+  {
+    title: 'Your Personalized Career Plan',
+    subTitle: 'Built from your visa status, background, and career goals.',
+    path: ROUTE_PATH.DASHBOARD,
+  },
+  {
+    title: 'Road Map',
+    subTitle: 'Here is your career overview!',
+    path: ROUTE_PATH.ROADMAP,
+  },
+  {
+    title: 'Fit analysis',
+    subTitle: 'Here is your career overview!', // 변경 예정
+    path: ROUTE_PATH.FITANALYSIS,
+  },
+];
+
 const Header = () => {
-  return <header className={styles.container}>Header</header>;
+  // 추후 api response로 대체
+  const mockUser = {
+    username: 'Yoonseo Bong',
+    profileUrl:
+      'https://www.biteme.co.kr/blog/wp-content/uploads/2025/04/491b87a0-1913-43ab-92e3-b16c2a1e6f82.jpg',
+  };
+  // const mockUser = {
+  //   username: undefined,
+  //   profileUrl: undefined,
+  // };
+  const location = useLocation();
+
+  return (
+    <header className={styles.container}>
+      {TITLE_LIST.map(({ title, subTitle, path }) => {
+        if (location.pathname === path) {
+          const showUsername =
+            path === ROUTE_PATH.DASHBOARD && mockUser.username;
+
+          return (
+            <div key={path} className={styles.left_section}>
+              <h1 className={styles.title}>
+                {title}
+                {showUsername ? `, ${mockUser.username}!` : ''}
+              </h1>
+              <h2 className={styles.subTitle}>{subTitle}</h2>
+            </div>
+          );
+        }
+      })}
+
+      {mockUser.profileUrl ? (
+        <img
+          src={mockUser.profileUrl}
+          alt="사용자 프로필 이미지"
+          className={styles.img}
+        />
+      ) : (
+        <UserProfileIcon />
+      )}
+    </header>
+  );
 };
 
 export default Header;
