2638487e64786cdad8b56760f64e169c9d2c36a7
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-01-13 23:29:32 +0900

    refactor: bar 컴포넌트 -> title 컴포넌트로 합치기

--- apps/web/src/entities/phase/ui/career-roadmap-step-title/career-roadmap-step-title.css.ts
+++ apps/web/src/entities/phase/ui/career-roadmap-step-title/career-roadmap-step-title.css.ts
@@ -1,14 +1,31 @@
 import { themeVars, typography } from '@kds/ui/styles';
+import { createVar, style } from '@vanilla-extract/css';
 import { recipe } from '@vanilla-extract/recipes';
 
-export const container = recipe({
+const titleColor = createVar();
+const timeColor = createVar();
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '1.2rem',
+  width: 'fit-content',
+});
+
+export const button = recipe({
   base: {
     display: 'flex',
     flexDirection: 'column',
+    width: 'fit-content',
     padding: '1.2rem 1.6rem',
     border: '1px solid',
     borderRadius: '8px',
     backgroundColor: themeVars.color.grayscale.white,
+    vars: {
+      [titleColor]: themeVars.color.primary[500],
+      [timeColor]: themeVars.color.primary[400],
+    },
   },
   variants: {
     isActive: {
@@ -24,6 +41,10 @@ export const container = recipe({
       },
       false: {
         borderColor: themeVars.color.primary[200],
+        vars: {
+          [titleColor]: themeVars.color.primary[400],
+          [timeColor]: themeVars.color.primary[300],
+        },
         ':hover': {
           borderColor: themeVars.color.primary[300],
           backgroundColor: themeVars.color.primary[100],
@@ -31,45 +52,74 @@ export const container = recipe({
         ':active': {
           borderColor: themeVars.color.primary[400],
           backgroundColor: themeVars.color.primary[200],
+          vars: {
+            [titleColor]: themeVars.color.primary[500],
+            [timeColor]: themeVars.color.primary[400],
+          },
         },
       },
     },
   },
 });
 
-export const title = recipe({
+export const title = style({
+  ...typography.body7_sb_14,
+  color: titleColor,
+});
+
+export const time = style({
+  ...typography.cap2_m_12,
+  color: timeColor,
+});
+
+export const stepbar = style({
+  width: '22rem',
+  padding: '1rem 0',
+  position: 'relative',
+});
+
+export const line = recipe({
+  base: {
+    width: '100%',
+    height: '0.4rem',
+    borderRadius: '100px',
+  },
+  variants: {
+    isActive: {
+      true: {
+        backgroundColor: themeVars.color.primary[500],
+      },
+      false: {
+        backgroundColor: themeVars.color.primary[200],
+      },
+    },
+  },
+});
+
+export const circle = recipe({
   base: {
     ...typography.body7_sb_14,
+    width: '2.4rem',
+    height: '2.4rem',
+    border: `2px solid transparent`,
+    borderRadius: '10px',
+    textAlign: 'center',
+    lineHeight: '2rem',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
   },
   variants: {
     isActive: {
       true: {
-        color: themeVars.color.primary[500],
+        backgroundColor: themeVars.color.primary[500],
+        color: themeVars.color.grayscale.white,
       },
       false: {
+        backgroundColor: themeVars.color.primary[100],
         color: themeVars.color.primary[400],
-        ':active': {
-          color: themeVars.color.primary[500],
-        },
-      },
-    },
-  },
-});
-
-export const time = recipe({
-  base: {
-    ...typography.cap2_m_12,
-  },
-  variants: {
-    isActive: {
-      true: {
-        color: themeVars.color.primary[400],
-      },
-      false: {
-        color: themeVars.color.primary[300],
-        ':active': {
-          color: themeVars.color.primary[400],
-        },
+        borderColor: themeVars.color.primary[200],
       },
     },
   },
--- apps/web/src/entities/phase/ui/career-roadmap-step-title/career-roadmap-step-title.tsx
+++ apps/web/src/entities/phase/ui/career-roadmap-step-title/career-roadmap-step-title.tsx
@@ -3,19 +3,27 @@ import * as styles from './career-roadmap-step-title.css';
 interface CareerRoadmapStepTitleProps {
   title: string;
   period: string;
+  phase: number;
   isActive: boolean;
 }
 
 const CareerRoadmapStepTitle = ({
   title,
   period,
+  phase,
   isActive,
 }: CareerRoadmapStepTitleProps) => {
   return (
-    <button type="button" className={styles.container({ isActive: isActive })}>
-      <h3 className={styles.title({ isActive: isActive })}>{title}</h3>
-      <time className={styles.time({ isActive: isActive })}>{period}</time>
-    </button>
+    <div className={styles.container}>
+      <button type="button" className={styles.button({ isActive: isActive })}>
+        <h3 className={styles.title}>{title}</h3>
+        <time className={styles.time}>{period}</time>
+      </button>
+      <div className={styles.stepbar}>
+        <div className={styles.line({ isActive: isActive })} />
+        <div className={styles.circle({ isActive: isActive })}>{phase}</div>
+      </div>
+    </div>
   );
 };
 
