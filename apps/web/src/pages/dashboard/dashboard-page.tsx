74f3006e737823415eebf2eeafd2cc7ce3da4fda
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-01-14 00:07:03 +0900

    chore: 코드 리뷰를 위한 임시 코드 추가

--- apps/web/src/pages/dashboard/dashboard-page.tsx
+++ apps/web/src/pages/dashboard/dashboard-page.tsx
@@ -1,5 +1,44 @@
+import { useState } from 'react';
+
+import { CareerRoadmapStep, CareerRoadmapStepInfo } from '@entities/phase';
+
 const DashboardPage = () => {
-  return <div>Dashboard Page</div>;
+  const [clickedPhase, setClickedPhase] = useState(0);
+
+  return (
+    <div>
+      <CareerRoadmapStep
+        title="OPT Preparation"
+        period="Sep. 25 - Nov. 25"
+        phase={0}
+        onClick={() => setClickedPhase(0)}
+        isActive={clickedPhase === 0}
+        bottom={
+          <CareerRoadmapStepInfo
+            status="Past"
+            label="Incompleted works"
+            workscount={2}
+            isActive={clickedPhase === 0}
+          />
+        }
+      />
+      <CareerRoadmapStep
+        title="OPT Preparation"
+        period="Sep. 25 - Nov. 25"
+        phase={1}
+        onClick={() => setClickedPhase(1)}
+        isActive={clickedPhase === 1}
+        bottom={
+          <CareerRoadmapStepInfo
+            status="Current"
+            label="Remained works"
+            workscount={2}
+            isActive={clickedPhase === 1}
+          />
+        }
+      />
+    </div>
+  );
 };
 
 export default DashboardPage;
