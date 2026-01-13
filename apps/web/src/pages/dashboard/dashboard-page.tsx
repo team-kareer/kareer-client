67cb1f0dd3c08d7b5c0e0d203132bb3c6b0b0945
jeonghoon11 <jih361@naver.com>
2026-01-13 18:53:06 +0900

    chore: 예시코드 추가

--- apps/web/src/pages/dashboard/dashboard-page.tsx
+++ apps/web/src/pages/dashboard/dashboard-page.tsx
@@ -9,12 +9,32 @@ const DashboardPage = () => {
         icon={<TodoCheckIcon width={24} height={24} />}
         title="Write your todo here"
         description="Due in XX days"
+        size="lg"
+        checked={true}
       />
 
       <TodoItem
         icon={<TodoIcon width={24} height={24} />}
         title="Write your todo here"
         description="Due in XX days"
+        size="lg"
+        checked={false}
+      />
+
+      <TodoItem
+        icon={<TodoIcon width={24} height={24} />}
+        title="Write your todo here"
+        description="Due in XX days"
+        size="sm"
+        checked={false}
+      />
+
+      <TodoItem
+        icon={<TodoCheckIcon width={24} height={24} />}
+        title="Write your todo here"
+        description="Due in XX days"
+        size="sm"
+        checked={true}
       />
     </div>
   );
