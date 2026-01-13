f2bd11f6d9b1e34252f2f6de48d1a3bee0b52086
jeonghoon11 <jih361@naver.com>
2026-01-14 00:29:21 +0900

    fix: 서비스 전역 레이아웃 height 고정

--- packages/kds-ui/src/styles/global.css.ts
+++ packages/kds-ui/src/styles/global.css.ts
@@ -1,10 +1,11 @@
 import { globalStyle } from '@vanilla-extract/css';
 
+import { height } from './tokens/height.css';
 import { width } from './tokens/width.css';
 
 globalStyle('html, body', {
-  minWidth: width.minWidth,
-  height: '100dvh',
+  minWidth: '100vw',
+  minHeight: '100vh',
   overflow: 'hidden',
   padding: '0',
   fontSize: '62.5%',
@@ -14,9 +15,11 @@ globalStyle('html, body', {
 globalStyle('body', {
   display: 'flex',
   justifyContent: 'center',
+  alignItems: 'center',
 });
 
 globalStyle('#root', {
   width: width.minWidth,
+  height: height.minHeight,
   boxShadow: '0 0 24px rgba(0, 0, 0, 0.12)',
 });
