29d502aa9a7686c7859a2d9f598ddd7a7a7f5664
yooncandooit <dotsi0@dgu.ac.kr>
2026-01-14 05:43:27 +0900

    refactor: 태그 컴포넌트 속성 사용

--- apps/web/src/entities/phase/ui/required-action-card/required-action-card.tsx
+++ apps/web/src/entities/phase/ui/required-action-card/required-action-card.tsx
@@ -1,16 +1,24 @@
+import { ComponentProps } from 'react';
 import { Tag } from '@kds/ui';
 
 import * as styles from './required-action-card.css';
 
 interface RequiredActionCardProps {
+  tagColor: ComponentProps<typeof Tag>['color'];
+  tagLabel: 'Visa' | 'Career';
   title: string;
   dueDate: string;
 }
 
-const RequiredActionCard = ({ title, dueDate }: RequiredActionCardProps) => {
+const RequiredActionCard = ({
+  tagColor,
+  tagLabel,
+  title,
+  dueDate,
+}: RequiredActionCardProps) => {
   return (
     <article className={styles.container}>
-      <Tag color={'pastel_purple'} children="Visa" />
+      <Tag color={tagColor}>{tagLabel}</Tag>
       <article className={styles.content}>
         <h3 className={styles.title}>{title}</h3>
         <p className={styles.dueDate}>{dueDate}</p>
