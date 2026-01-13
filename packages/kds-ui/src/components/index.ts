4f31bcaae43a759c4795d0c062a7700d2a07d16f
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-01-13 23:30:28 +0900

    refactor: 컴포넌트 합치기로 인해 불필요해진 코드 삭제

--- packages/kds-ui/src/components/index.ts
+++ packages/kds-ui/src/components/index.ts
@@ -1,5 +1,4 @@
 export { default as Avatar } from './avatar/avatar';
 export { default as Button } from './button/button';
-export { default as CareerRoadmapBar } from './career-roadmap-bar/career-roadmap-bar';
 export { default as SectionHeader } from './section-header/section-header';
 export { default as Tag } from './tag/tag';
