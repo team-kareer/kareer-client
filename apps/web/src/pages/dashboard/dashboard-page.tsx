22cf5a4532089bd6b77e227271ed1b70066cd2f2
jeonghoon11 <jih361@naver.com>
2026-01-13 19:30:41 +0900

    chore: 예시코드 작성

--- apps/web/src/pages/dashboard/dashboard-page.tsx
+++ apps/web/src/pages/dashboard/dashboard-page.tsx
@@ -1,34 +1,27 @@
+import { useState } from 'react';
+
 import TodoItem from '@entities/todo/ui/todo-item/todo-item';
 
 const DashboardPage = () => {
+  const [lgChecked, setLgChecked] = useState(false);
+  const [smChecked, setSmChecked] = useState(false);
+
   return (
     <div style={{ display: 'flex', gap: '1.6rem' }}>
       <TodoItem
         title="Write your todo here"
         description="Due in XX days"
         size="lg"
-        isChecked={true}
-      />
-
-      <TodoItem
-        title="Write your todo here"
-        description="Due in XX days"
-        size="lg"
-        isChecked={false}
+        isChecked={lgChecked}
+        onClick={() => setLgChecked((prev) => !prev)}
       />
 
       <TodoItem
         title="Write your todo here"
         description="Due in XX days"
         size="sm"
-        isChecked={false}
-      />
-
-      <TodoItem
-        title="Write your todo here"
-        description="Due in XX days"
-        size="sm"
-        isChecked={true}
+        isChecked={smChecked}
+        onClick={() => setSmChecked((prev) => !prev)}
       />
     </div>
   );
