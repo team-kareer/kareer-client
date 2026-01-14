2fb5b664310de15fb5f79d83b0dc417de49ecf51
jeonghoon11 <jih361@naver.com>
2026-01-15 01:45:45 +0900

    chore: 예시코드 추가

--- apps/web/src/pages/dashboard/dashboard-page.tsx
+++ apps/web/src/pages/dashboard/dashboard-page.tsx
@@ -1,5 +1,21 @@
+import { useState } from 'react';
+
+import TodoItem from '@entities/todo/ui/todo-item/todo-item';
+
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  const [isChecked, setIsChecked] = useState(false);
+
+  return (
+    <div>
+      <TodoItem
+        title="하이"
+        description="하이하이하이"
+        size="sm"
+        isChecked={isChecked}
+        onToggle={() => setIsChecked((prev) => !prev)}
+      />
+    </div>
+  );
 };
 
 export default DashboardPage;
