602d375298deda3eba8e6a8e11e45a616ed12c61
yooncandooit <dotsi0@dgu.ac.kr>
2026-01-14 21:01:42 +0900

    fix: 타입 오류 해결

--- packages/kds-ui/src/components/index.ts
+++ packages/kds-ui/src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as TextField } from '../../../../apps/web/src/shared/ui/text-field/text-field';
+export { default as Avatar } from './avatar/avatar';
 export { default as Button } from './button/button';
 export { default as SectionHeader } from './section-header/section-header';
 export { default as Tag } from './tag/tag';
