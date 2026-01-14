01e8717f5b0fe52d86507d00f4c4d7084731be4e
jeonghoon11 <jih361@naver.com>
2026-01-15 01:45:29 +0900

    feat: checkbox 컴포넌트 구현

---
+++ packages/kds-ui/src/components/checkbox/checkbox.tsx
@@ -0,0 +1,24 @@
+import { TodoCheckIcon, TodoIcon } from '@kds/icons';
+import type { ButtonHTMLAttributes } from 'react';
+
+import * as styles from './checkbox.css';
+
+interface CheckboxProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  isChecked: boolean;
+}
+
+const Checkbox = ({ isChecked, ...props }: CheckboxProps) => {
+  const todoIcon = isChecked ? (
+    <TodoCheckIcon width={24} height={24} />
+  ) : (
+    <TodoIcon width={24} height={24} />
+  );
+
+  return (
+    <button className={styles.icon} {...props}>
+      {todoIcon}
+    </button>
+  );
+};
+
+export default Checkbox;
