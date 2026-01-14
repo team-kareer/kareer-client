650f03e0f2619a672c9ba79674bc51898e8c7164
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-01-15 05:53:37 +0900

    feat: Action Required Accordion 구현

---
+++ apps/web/src/widgets/dashboard/ui/action-required-accordion/action-required-accordion.css.ts
@@ -0,0 +1,74 @@
+import { themeVars, typography } from '@kds/ui/styles';
+import { style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '1.6rem 2rem',
+  borderRadius: '10px',
+  backgroundColor: themeVars.color.grayscale.gray100,
+});
+
+export const header = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+});
+
+export const left_section = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.8rem',
+});
+
+export const text = recipe({
+  base: {
+    ...typography.body7_sb_14,
+    userSelect: 'none',
+  },
+  variants: {
+    textTone: {
+      gray: {
+        color: themeVars.color.grayscale.gray800,
+      },
+      primary: {
+        color: themeVars.color.primary[500],
+      },
+    },
+  },
+});
+
+export const icon = style({
+  cursor: 'pointer',
+});
+
+export const wrapper = recipe({
+  base: {
+    display: 'grid',
+    transition: 'grid-template-rows 0.3s ease-out',
+  },
+  variants: {
+    isOpen: {
+      true: {
+        gridTemplateRows: '1fr',
+      },
+      false: {
+        gridTemplateRows: '0fr',
+      },
+    },
+  },
+});
+
+export const inner = style({
+  minHeight: '0',
+  overflow: 'hidden',
+});
+
+export const content = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.8rem',
+  marginTop: '1.6rem',
+  maxHeight: '16.2rem',
+  overflowY: 'scroll',
+});
---
+++ apps/web/src/widgets/dashboard/ui/action-required-accordion/action-required-accordion.tsx
@@ -0,0 +1,100 @@
+import { ArrowDownIcon, ArrowUpIcon } from '@kds/icons';
+import { themeVars } from '@kds/ui/styles';
+
+import { RequiredActionCard } from '@entities/phase';
+import { useAccordion } from '@shared/hooks/useAccordion';
+
+import * as styles from './action-required-accordion.css';
+
+interface ActionRequiredAccordion {
+  counts: number;
+}
+
+interface mock {
+  tagType: 'Visa' | 'Career';
+  title: string;
+  dueDate: string;
+}
+
+const ActionRequiredAccordion = ({ counts }: ActionRequiredAccordion) => {
+  // 추후 api 응답 값으로 변경
+  const mockData: mock[] = [
+    {
+      tagType: 'Visa',
+      title: 'Prepare OPT application documents',
+      dueDate: 'Jan 25, 2026',
+    },
+    {
+      tagType: 'Visa',
+      title: 'Prepare OPT application documents',
+      dueDate: 'Jan 25, 2026',
+    },
+    {
+      tagType: 'Visa',
+      title: 'Prepare OPT application documents',
+      dueDate: 'Jan 25, 2026',
+    },
+    {
+      tagType: 'Career',
+      title: 'Prepare OPT application documents',
+      dueDate: 'Jan 25, 2026',
+    },
+    {
+      tagType: 'Career',
+      title: 'Prepare OPT application documents',
+      dueDate: 'Jan 25, 2026',
+    },
+  ];
+  const { isOpen, shouldRender, toggle } = useAccordion();
+
+  return (
+    // accordionItem
+    <section className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.left_section}>
+          <h3 className={styles.text({ textTone: 'gray' })}>Action Required</h3>
+          <span className={styles.text({ textTone: 'primary' })}>{counts}</span>
+        </div>
+
+        {/* accordionTrigger */}
+        {isOpen ? (
+          <ArrowUpIcon
+            width={16}
+            height={16}
+            color={themeVars.color.grayscale.gray800}
+            className={styles.icon}
+            onClick={toggle}
+          />
+        ) : (
+          <ArrowDownIcon
+            width={16}
+            height={16}
+            color={themeVars.color.grayscale.gray800}
+            className={styles.icon}
+            onClick={toggle}
+          />
+        )}
+      </header>
+
+      {/* accordionContent */}
+      <div className={styles.wrapper({ isOpen: isOpen })}>
+        <div className={styles.inner}>
+          {shouldRender && (
+            <div className={styles.content}>
+              {mockData.map((data, idx) => (
+                <RequiredActionCard
+                  key={`${data.tagType}-${idx}`}
+                  tagType={data.tagType}
+                  title={data.title}
+                  dueDate={data.dueDate}
+                />
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ActionRequiredAccordion;
