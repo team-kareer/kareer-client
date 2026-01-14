bef58a17577564d10136c26b5b4c1b15bbd9c7d9
Yoonji Kim <161998554+yooncandooit@users.noreply.github.com>
2026-01-14 21:16:55 +0900

    Feat(kds): TextField 공통 컴포넌트 구현 (#71)
    * chore: gitkeep 파일 삭제
    
    * feat: textfield ui 구현
    
    * feat: text-field 공통 컴포넌트 수정
    
    * fix: forwardRef 제거 및 예시 코드 구조 개선
    
    * fix: 컬러 토큰을 themeVars로 교체
    
    * fix: shared/ui로 경로 이동
    
    * fix: tag 컴포넌트 컬러 토큰 themeVars로 수정
    
    * fix: kds/ui 경로 파일 삭제
    
    * fix: 타입 오류 해결
    
    * chore: 테스트 코드 삭제

--- packages/kds-ui/src/components/index.ts
+++ packages/kds-ui/src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as TextField } from '../../../../apps/web/src/shared/ui/text-field/text-field';
 export { default as Avatar } from './avatar/avatar';
 export { default as Button } from './button/button';
 export { default as SectionHeader } from './section-header/section-header';
