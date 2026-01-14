5ff4b7e7f93380427db04a613c4366d12bc869f1
yooncandooit <dotsi0@dgu.ac.kr>
2026-01-15 04:35:18 +0900

    feat: input 컴포넌트 ui 구현

--- packages/kds-ui/src/components/index.ts
+++ packages/kds-ui/src/components/index.ts
@@ -1,6 +1,6 @@
-export { default as TextField } from '../../../../apps/web/src/shared/ui/text-field/text-field';
 export { default as Avatar } from './avatar/avatar';
 export { default as Button } from './button/button';
 export { default as Checkbox } from './checkbox/checkbox';
+export { default as Input } from './input/input';
 export { default as SectionHeader } from './section-header/section-header';
 export { default as Tag } from './tag/tag';
