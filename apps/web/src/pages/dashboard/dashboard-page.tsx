0da0be9ac45dfbb9a2a4b66e59017c1c964773a3
huniversal <hunivers0523@naver.com>
2026-01-16 02:20:18 +0900

    test: 테스트 코드 제거

--- apps/web/src/pages/dashboard/dashboard-page.tsx
+++ apps/web/src/pages/dashboard/dashboard-page.tsx
@@ -1,43 +1,5 @@
-import { OnboardingStep } from '@widgets/onboarding';
-
-const ONBOARDING_STEPS = [
-  {
-    stepNumber: 1,
-    title: 'Personal Information',
-    status: 'In Progress',
-  },
-  {
-    stepNumber: 2,
-    title: 'Visa Information',
-    status: 'Next',
-  },
-  {
-    stepNumber: 3,
-    title: 'Target Role',
-    status: 'Later',
-  },
-  {
-    stepNumber: 4,
-    title: 'Background',
-    status: 'Later',
-  },
-] as const;
-
-type OnboardingStep = (typeof ONBOARDING_STEPS)[number];
-
 const DashboardPage = () => {
-  return (
-    <div style={{ display: 'flex', gap: '5rem' }}>
-      {ONBOARDING_STEPS.map((step) => (
-        <OnboardingStep
-          key={step.stepNumber}
-          stepNumber={step.stepNumber}
-          title={step.title}
-          status={step.status}
-        />
-      ))}
-    </div>
-  );
+  return <div>DashboardPage</div>;
 };
 
 export default DashboardPage;
