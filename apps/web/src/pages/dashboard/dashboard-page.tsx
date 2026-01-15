d3c9e87c3dc69646a46e1dd7c2ad9ee49b17ff5c
jeonghoon11 <jih361@naver.com>
2026-01-16 01:09:19 +0900

    chore: 예시 코드 추가

--- apps/web/src/pages/dashboard/dashboard-page.tsx
+++ apps/web/src/pages/dashboard/dashboard-page.tsx
@@ -1,5 +1,19 @@
+import UploadBox from '@widgets/fit-analysis/ui/upload-box/upload-box';
+
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        backgroundColor: 'blue',
+        padding: '20px',
+        height: '200px',
+      }}
+    >
+      <UploadBox />
+    </div>
+  );
 };
 
 export default DashboardPage;
