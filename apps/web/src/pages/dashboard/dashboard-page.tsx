8a6c3e11e2fc7a8271c5d950807d4f14b9c9f243
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-01-16 03:45:35 +0900

    chore: 테스트 코드 추가

--- apps/web/src/pages/dashboard/dashboard-page.tsx
+++ apps/web/src/pages/dashboard/dashboard-page.tsx
@@ -1,5 +1,12 @@
+import { VisaStatusBox } from '@shared/ui';
+
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <div>
+      <VisaStatusBox isCurrent={true} goal="D-2 Student" percent={79} />
+      <VisaStatusBox isCurrent={false} goal="D-10 Job Seeker" percent={32} />
+    </div>
+  );
 };
 
 export default DashboardPage;
