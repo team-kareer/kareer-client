4f0a472c9333f9333895e16738aeb0c677533601
huniversal <hunivers0523@naver.com>
2026-01-15 19:06:03 +0900

    feat: 테스트 코드 제거

--- apps/web/src/pages/dashboard/dashboard-page.tsx
+++ apps/web/src/pages/dashboard/dashboard-page.tsx
@@ -1,49 +1,5 @@
-import { useState } from 'react';
-import { Autocomplete } from '@kds/ui';
-
-const MAJOR_OPTIONS = [
-  'Frontend Developer',
-  'Backend Developer',
-  'Fullstack Developer',
-  'Mobile Developer',
-  'UI/UX Designer',
-  'Product Manager',
-  'Scrum Master',
-  'DevOps Engineer',
-  'Data Scientist',
-  'AI Engineer',
-  'Cybersecurity Engineer',
-  'Game Developer',
-  'Blockchain Developer',
-  'System Administrator',
-  'Network Engineer',
-  'Database Administrator',
-  'Security Analyst',
-];
-
 const DashboardPage = () => {
-  const [value, setValue] = useState('');
-
-  return (
-    <div
-      style={{
-        padding: '2rem',
-        width: '400px',
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '2rem',
-        backgroundColor: 'lightgray',
-      }}
-    >
-      <Autocomplete
-        value={value}
-        onChange={setValue}
-        options={MAJOR_OPTIONS}
-        placeholder="Enter your major"
-      />
-    </div>
-  );
+  return <div>DashboardPage</div>;
 };
 
 export default DashboardPage;
