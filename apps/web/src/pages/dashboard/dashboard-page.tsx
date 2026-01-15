971a06effcaddbb2ed86901f3ae89024177d14ef
huniversal <hunivers0523@naver.com>
2026-01-15 23:47:48 +0900

    feat: Step 구현

--- apps/web/src/pages/dashboard/dashboard-page.tsx
+++ apps/web/src/pages/dashboard/dashboard-page.tsx
@@ -1,5 +1,43 @@
+import OnboardingStep from '@widgets/onboarding/ui/step/onboarding-step';
+
+const ONBOARDING_STEPS = [
+  {
+    stepNumber: 1,
+    title: 'Personal Information',
+    status: 'In Progress',
+  },
+  {
+    stepNumber: 2,
+    title: 'Visa Information',
+    status: 'Next',
+  },
+  {
+    stepNumber: 3,
+    title: 'Target Role',
+    status: 'Later',
+  },
+  {
+    stepNumber: 4,
+    title: 'Background',
+    status: 'Later',
+  },
+] as const;
+
+type OnboardingStep = (typeof ONBOARDING_STEPS)[number];
+
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <div style={{ display: 'flex', gap: '5rem' }}>
+      {ONBOARDING_STEPS.map((step) => (
+        <OnboardingStep
+          key={step.stepNumber}
+          stepNumber={step.stepNumber}
+          title={step.title}
+          status={step.status}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default DashboardPage;
---
+++ apps/web/src/widgets/onboarding/ui/step/onboarding-step.tsx
@@ -0,0 +1,29 @@
+import * as styles from './onboarding-step.css';
+import { Tag } from '@kds/ui';
+
+interface OnboardingStepProps {
+  stepNumber: number;
+  title: string;
+  status: 'In Progress' | 'Next' | 'Later' | 'Completed';
+}
+
+const OnboardingStep = ({ stepNumber, title, status }: OnboardingStepProps) => {
+  const tagColor = {
+    'In Progress': 'primary_blue',
+    Next: 'outlined_black',
+    Later: 'disabled_gray',
+    Completed: 'disabled_gray',
+  } as const;
+
+  return (
+    <div className={styles.container}>
+      <p className={styles.stepNumber({ status })}>STEP {stepNumber}</p>
+      <h2 className={styles.title({ status })}>{title}</h2>
+      <div className={styles.tagWrapper}>
+        <Tag color={tagColor[status]}>{status}</Tag>
+      </div>
+    </div>
+  );
+};
+
+export default OnboardingStep;
