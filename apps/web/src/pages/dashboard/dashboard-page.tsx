992f7fb2ba69462f2644cbe640c7562132ba6e45
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-01-15 15:37:05 +0900

    chore: 코드 리뷰를 위한 임시 코드 추가

--- apps/web/src/pages/dashboard/dashboard-page.tsx
+++ apps/web/src/pages/dashboard/dashboard-page.tsx
@@ -1,5 +1,11 @@
+import { ActionRequiredAccordion } from '@widgets/dashboard/ui';
+
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <div>
+      <ActionRequiredAccordion counts={7} />
+    </div>
+  );
 };
 
 export default DashboardPage;
