6b6ae62e618772a2ebc10a94bab86fba7c46c951
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-01-17 04:59:18 +0900

    chore: 테스트 코드 추가

--- apps/web/src/pages/dashboard/dashboard-page.tsx
+++ apps/web/src/pages/dashboard/dashboard-page.tsx
@@ -1,5 +1,12 @@
+import { VisaEligibility } from '@widgets/fit-analysis';
+
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <div>
+      <VisaEligibility visa={'D-2 Student'} />
+      <VisaEligibility visa={'D-10 Job Seeker'} />
+    </div>
+  );
 };
 
 export default DashboardPage;
