a0472ef3512119b0c040113ca34c9ed583b51892
jeonghoon11 <jih361@naver.com>
2026-01-17 05:48:33 +0900

    chore: 예시코드 제거

--- apps/web/src/pages/dashboard/dashboard-page.tsx
+++ apps/web/src/pages/dashboard/dashboard-page.tsx
@@ -1,19 +1,5 @@
-import UploadBox from '@widgets/fit-analysis/ui/upload-box/upload-box';
-
 const DashboardPage = () => {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        backgroundColor: 'blue',
-        padding: '20px',
-        height: '200px',
-      }}
-    >
-      <UploadBox />
-    </div>
-  );
+  return <div>DashboardPage</div>;
 };
 
 export default DashboardPage;
