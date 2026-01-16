09b1bdd1a59835e28b7a2a49492d4b2e73e1debd
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-01-17 04:58:20 +0900

    refactor: 파일명 변경

--- apps/web/src/widgets/fit-analysis/index.ts
+++ apps/web/src/widgets/fit-analysis/index.ts
@@ -1,6 +1,7 @@
-export { VISA_CHECKLIST_D2 } from './types/visa-checklist';
-export { VISA_CHECKLIST_D10 } from './types/visa-checklist';
-export { VISA_CHECKLIST_E7 } from './types/visa-checklist';
+export { VISA_CHECKLIST_D2 } from './types/visa-eligibility';
+export { VISA_CHECKLIST_D10 } from './types/visa-eligibility';
+export { VISA_CHECKLIST_E7 } from './types/visa-eligibility';
+export { VISA_LIST } from './types/visa-eligibility';
 export { default as VisaChecklist } from './ui/visa-checklist/visa-checklist';
 export { default as VisaEligibility } from './ui/visa-eligibility/visa-eligibility';
 export { default as VisaTabList } from './ui/visa-tab-list/visa-tab-list';
--- apps/web/src/widgets/fit-analysis/types/visa-eligibility.ts
+++ apps/web/src/widgets/fit-analysis/types/visa-eligibility.ts
@@ -1,3 +1,9 @@
+export const VISA_LIST = {
+  D2: 'D-2 Student',
+  D10: 'D-10 Job Seeker',
+  E7: 'E-7 Foreign National of Special Ability',
+} as const;
+
 export const VISA_CHECKLIST_D2 = [
   {
     id: 1,
