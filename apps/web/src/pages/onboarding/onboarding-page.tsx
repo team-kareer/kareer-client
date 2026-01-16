24e3f25a1d077b68453eaa9ffb3295d535f4a82f
huniversal <hunivers0523@naver.com>
2026-01-16 16:49:40 +0900

    feat: 테스트 코드 제거

--- apps/web/src/pages/onboarding/onboarding-page.tsx
+++ apps/web/src/pages/onboarding/onboarding-page.tsx
@@ -1,37 +1,5 @@
-import {
-  type OnboardingStepData,
-  OnboardingStepHeader,
-} from '@widgets/onboarding';
-
-const ONBOARDING_STEPS: OnboardingStepData[] = [
-  {
-    stepNumber: 1,
-    title: 'Personal Information',
-    status: 'In Progress',
-  },
-  {
-    stepNumber: 2,
-    title: 'Visa Information',
-    status: 'Next',
-  },
-  {
-    stepNumber: 3,
-    title: 'Target Role',
-    status: 'Later',
-  },
-  {
-    stepNumber: 4,
-    title: 'Background',
-    status: 'Later',
-  },
-];
-
 const OnboardingPage = () => {
-  return (
-    <div>
-      <OnboardingStepHeader steps={ONBOARDING_STEPS} />
-    </div>
-  );
+  return <div>OnboardingPage</div>;
 };
 
 export default OnboardingPage;
