ca752406b6adc543f755f2b9e4b7a011932a5a83
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-01-17 05:20:07 +0900

    chore: export 경로 수정

--- apps/web/src/widgets/fit-analysis/index.ts
+++ apps/web/src/widgets/fit-analysis/index.ts
@@ -1,7 +1,7 @@
-export { VISA_CHECKLIST_D2 } from './types/visa-eligibility';
-export { VISA_CHECKLIST_D10 } from './types/visa-eligibility';
-export { VISA_CHECKLIST_E7 } from './types/visa-eligibility';
-export { VISA_LIST } from './types/visa-eligibility';
+export { VISA_LIST } from './constants/visa';
+export { VISA_CHECKLIST_D2 } from './constants/visa-checklist';
+export { VISA_CHECKLIST_D10 } from './constants/visa-checklist';
+export { VISA_CHECKLIST_E7 } from './constants/visa-checklist';
 export { default as VisaChecklist } from './ui/visa-checklist/visa-checklist';
 export { default as VisaEligibility } from './ui/visa-eligibility/visa-eligibility';
 export { default as VisaTabList } from './ui/visa-tab-list/visa-tab-list';
