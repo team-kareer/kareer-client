e23f0c34782e38847013f702cb0f57ba897899a6
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-01-17 04:28:10 +0900

    chore: export 구문 추가

---
+++ apps/web/src/widgets/fit-analysis/index.ts
@@ -0,0 +1,6 @@
+export { VISA_CHECKLIST_D2 } from './types/visa-checklist';
+export { VISA_CHECKLIST_D10 } from './types/visa-checklist';
+export { VISA_CHECKLIST_E7 } from './types/visa-checklist';
+export { default as VisaChecklist } from './ui/visa-checklist/visa-checklist';
+export { default as VisaEligibility } from './ui/visa-eligibility/visa-eligibility';
+export { default as VisaTabList } from './ui/visa-tab-list/visa-tab-list';
