3777ba5c7a3ac045c3e993268a5e0c110a6f17dc
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-01-17 04:26:58 +0900

    feat: visa eligibility 컴포넌트 구현

---
+++ apps/web/src/widgets/fit-analysis/ui/visa-eligibility/visa-eligibility.tsx
@@ -0,0 +1,48 @@
+import { Tab } from '@kds/ui';
+
+import {
+  VISA_CHECKLIST_D2,
+  VISA_CHECKLIST_D10,
+  VISA_CHECKLIST_E7,
+  VisaChecklist,
+  VisaTabList,
+} from '@widgets/fit-analysis';
+
+import * as styles from './visa-eligibility.css';
+
+interface VisaEligibilityProps {
+  visa: string;
+}
+
+const VISA_LIST = {
+  D2: 'D-2 Student',
+  D10: 'D-10 Job Seeker',
+  E7: 'E-7 Foreign National of Special Ability',
+} as const;
+
+const VISA_TAB_PANELS = [
+  { tab: VISA_LIST.D2, checklistData: VISA_CHECKLIST_D2 },
+  { tab: VISA_LIST.D10, checklistData: VISA_CHECKLIST_D10 },
+  { tab: VISA_LIST.E7, checklistData: VISA_CHECKLIST_E7 },
+] as const;
+
+const VisaEligibility = ({ visa }: VisaEligibilityProps) => {
+  return (
+    <Tab.Container initialValue={visa} className={styles.container}>
+      <Tab.List className={styles.tabContainer}>
+        <VisaTabList visa={visa} />
+      </Tab.List>
+      {VISA_TAB_PANELS.map(({ tab, checklistData }) => (
+        <Tab.Panel key={tab} className={styles.panelContainer} tab={tab}>
+          <VisaChecklist
+            isCurrentVisa={visa === tab}
+            curTab={tab}
+            checklistData={checklistData}
+          />
+        </Tab.Panel>
+      ))}
+    </Tab.Container>
+  );
+};
+
+export default VisaEligibility;
