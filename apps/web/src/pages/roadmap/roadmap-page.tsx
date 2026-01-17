cc657187a1590cf367e99a9750fb0dbf279e0180
jeonghoon11 <jih361@naver.com>
2026-01-18 01:56:26 +0900

    feat: roadmap 페이지 구현

--- apps/web/src/pages/roadmap/roadmap-page.tsx
+++ apps/web/src/pages/roadmap/roadmap-page.tsx
@@ -1,5 +1,13 @@
+import CareerRoadmapSection from '@widgets/roadmap/ui/career-roadmap-section/career-roadmap-section';
+import PhaseListSection from '@widgets/roadmap/ui/phase-list-section/phase-list-section';
+
 const RoadmapPage = () => {
-  return <div>RoadmapPage</div>;
+  return (
+    <>
+      <CareerRoadmapSection />
+      <PhaseListSection />
+    </>
+  );
 };
 
 export default RoadmapPage;
