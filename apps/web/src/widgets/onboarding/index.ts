ade3fa56d43258fa571a4b308391aa839b2f4b6f
huniversal <hunivers0523@naver.com>
2026-01-17 16:51:42 +0900

    fix: import sort 이슈 해결

--- apps/web/src/widgets/onboarding/index.ts
+++ apps/web/src/widgets/onboarding/index.ts
@@ -1,7 +1,7 @@
+export { VALIDATION_MESSAGE } from './constants/validation-message';
+export { default as OnboardingDegreeStep } from './ui/onboarding-degree-step/onboarding-degree-step';
+export { default as OnboardingStep } from './ui/onboarding-step/onboarding-step';
 export { default as OnboardingStepHeader } from './ui/onboarding-step-headear/onboarding-step-header';
 export { type OnboardingStepData } from './ui/onboarding-step-headear/onboarding-step-header';
 export { default as OnboardingStepLayout } from './ui/onboarding-step-layout/onboarding-step-layout';
 export { default as OnboardingStepTitle } from './ui/onboarding-step-title/onboarding-step-title';
-export { default as OnboardingStep } from './ui/onboarding-step/onboarding-step';
-export { VALIDATION_MESSAGE } from './constants/validation-message';
-export { default as OnboardingDegreeStep } from './ui/onboarding-degree-step/onboarding-degree-step';
