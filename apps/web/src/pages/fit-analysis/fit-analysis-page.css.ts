53678f36a2eaa784936955cfd514b62702776dee
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-01-18 02:51:06 +0900

    feat: Fit Analysis Page 구현

---
+++ apps/web/src/pages/fit-analysis/fit-analysis-page.css.ts
@@ -0,0 +1,8 @@
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '2.4rem',
+  paddingBottom: '5.4rem',
+});
--- apps/web/src/pages/fit-analysis/fit-analysis-page.tsx
+++ apps/web/src/pages/fit-analysis/fit-analysis-page.tsx
@@ -1,5 +1,19 @@
+import { JobRecommendationList, VisaEligibility } from '@widgets/fit-analysis';
+import { Section } from '@shared/ui';
+
+import { container } from './fit-analysis-page.css';
+
 const FitAnalysisPage = () => {
-  return <div>FitAnalysisPage</div>;
+  return (
+    <div className={container}>
+      <Section title="Check visa eligibility">
+        <VisaEligibility />
+      </Section>
+      <Section title="Find Your Perfect Job Match">
+        <JobRecommendationList />
+      </Section>
+    </div>
+  );
 };
 
 export default FitAnalysisPage;
