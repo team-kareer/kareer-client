9e279496e04eed5c03420e82a01e91d5d854e86c
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-01-18 22:12:36 +0900

    refactor: page 컴포넌트 방식 변경

--- apps/web/src/pages/fit-analysis/fit-analysis-page.css.ts
+++
@@ -1,8 +0,0 @@
-import { style } from '@vanilla-extract/css';
-
-export const container = style({
-  display: 'flex',
-  flexDirection: 'column',
-  gap: '2.4rem',
-  paddingBottom: '5.4rem',
-});
--- apps/web/src/pages/fit-analysis/fit-analysis-page.tsx
+++ apps/web/src/pages/fit-analysis/fit-analysis-page.tsx
@@ -1,18 +1,11 @@
-import { JobRecommendationList, VisaEligibility } from '@widgets/fit-analysis';
-import { Section } from '@shared/ui';
-
-import { container } from './fit-analysis-page.css';
+import { UploadSection, VisaEligibilitySection } from '@widgets/fit-analysis';
 
 const FitAnalysisPage = () => {
   return (
-    <div className={container}>
-      <Section title="Check visa eligibility">
-        <VisaEligibility />
-      </Section>
-      <Section title="Find Your Perfect Job Match">
-        <JobRecommendationList />
-      </Section>
-    </div>
+    <>
+      <VisaEligibilitySection />
+      <UploadSection />
+    </>
   );
 };
 
