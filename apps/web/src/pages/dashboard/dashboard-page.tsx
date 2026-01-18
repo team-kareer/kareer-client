d598e07690bda51e8521a0b49006afcb9c661326
Yoonji Kim <dotsi0@dgu.ac.kr>
2026-01-19 04:54:37 +0900

    feat: 페이지 로더 구현

--- apps/web/src/pages/dashboard/dashboard-page.tsx
+++ apps/web/src/pages/dashboard/dashboard-page.tsx
@@ -1,13 +1,16 @@
-import { VisaStatusListSection } from '@widgets/dashboard/ui';
-import { MyBookmarkedJobsSection } from '@widgets/dashboard/ui';
-import { PhaseOverviewSection } from '@widgets/dashboard/ui';
+// import { VisaStatusListSection } from '@widgets/dashboard/ui';
+// import { MyBookmarkedJobsSection } from '@widgets/dashboard/ui';
+// import { PhaseOverviewSection } from '@widgets/dashboard/ui';
+
+import { PageLoader } from '@shared/ui';
 
 const DashboardPage = () => {
   return (
     <>
-      <VisaStatusListSection />
+      {/* <VisaStatusListSection />
       <PhaseOverviewSection />
-      <MyBookmarkedJobsSection />
+      <MyBookmarkedJobsSection /> */}
+      <PageLoader />
     </>
   );
 };
---
+++ apps/web/src/shared/ui/page-loading/page-loading.css.ts
@@ -0,0 +1,21 @@
+import { themeVars, typography } from '@kds/ui/styles';
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
+  height: '100vh',
+  flexDirection: 'column',
+});
+
+export const lottie = style({
+  width: '10rem',
+  height: '10rem',
+});
+
+export const text = style({
+  color: themeVars.color.grayscale.gray400,
+  ...typography.sub5_m_20,
+});
