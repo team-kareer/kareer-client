e23cdff42afe2f05abd1caa94b3a25d92813bc33
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-01-19 02:36:25 +0900

    fix: 충돌 해결

--- apps/web/src/pages/dashboard/dashboard-page.tsx
+++ apps/web/src/pages/dashboard/dashboard-page.tsx
@@ -1,18 +1,5 @@
-import { BookmarkedJobList } from '@features/job';
-// import { MOCK_JOBS } from '@shared/mocks/job-mocks';
-
 const DashboardPage = () => {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '2rem',
-      }}
-    >
-      <BookmarkedJobList jobs={[]} onScrap={() => {}} />
-    </div>
-  );
+  return <div>DashboardPage</div>;
 };
 
 export default DashboardPage;
