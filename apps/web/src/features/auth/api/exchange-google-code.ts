725d7230a36149569662a4e139ab572474b8d13e
jeonghoon11 <jih361@naver.com>
2026-01-19 03:31:21 +0900

    fix: 로그인 api 호출 방식 변경

--- apps/web/src/features/auth/api/exchange-google-code.ts
+++ apps/web/src/features/auth/api/exchange-google-code.ts
@@ -2,9 +2,12 @@ import { END_POINT } from '@features/auth/model/end_point';
 import { GoogleLoginResponse } from '@features/auth/model/types';
 import { api } from '@shared/apis/configs/instance';
 
-export const exchangeGoogleCode = (code: string) =>
-  api
+export const exchangeGoogleCode = async (code: string) => {
+  const response = await api
     .post(END_POINT.LOGIN.EXCHANGE_GOOGLE_CODE, {
       json: { code },
     })
     .json<GoogleLoginResponse>();
+
+  return response.data;
+};
