ef67999f8517be6941e2bc7f5e81193a7c923e3b
Yoonji Kim <dotsi0@dgu.ac.kr>
2026-01-19 01:24:19 +0900

    chore: 배럴 파일 수정

--- apps/web/src/pages/dashboard/dashboard-page.tsx
+++ apps/web/src/pages/dashboard/dashboard-page.tsx
@@ -1,6 +1,6 @@
 import { VisaStatusListSection } from '@widgets/dashboard/ui';
 import { MyBookmarkedJobsSection } from '@widgets/dashboard/ui';
-import PhaseOverviewSection from '@widgets/dashboard/ui/phase-overview-section/phase-overview-section';
+import { PhaseOverviewSection } from '@widgets/dashboard/ui';
 
 const DashboardPage = () => {
   return (
--- apps/web/src/widgets/dashboard/ui/index.ts
+++ apps/web/src/widgets/dashboard/ui/index.ts
@@ -2,5 +2,6 @@ export { default as Accordion } from './accordion/accordion';
 export { default as ActionRequiredAccordion } from './accordion/accordion';
 export { default as MyBookmarkedJobs } from './my-bookmarked-job/my-bookmarked-job';
 export { default as MyBookmarkedJobsSection } from './my-bookmarked-job-section/my-bookmarked-job-section';
+export { default as PhaseOverviewSection } from './phase-overview-section/phase-overview-section';
 export { default as VisaStatusList } from './visa-status-list/visa-status-list';
 export { default as VisaStatusListSection } from './visa-status-list-section/visa-status-list-section';
