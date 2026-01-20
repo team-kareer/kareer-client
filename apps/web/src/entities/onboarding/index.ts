79292a1887fd72e5cae9ed1d2a783a85126d9a56
huniversal <hunivers0523@naver.com>
2026-01-21 08:31:57 +0900

    fix: ESLint 수정

--- apps/web/src/entities/onboarding/index.ts
+++ apps/web/src/entities/onboarding/index.ts
@@ -1,5 +1,6 @@
 export * from './api';
 export * from './model/constants';
+export { END_POINT } from './model/end_point';
 export * from './model/form-to-request';
 export * from './model/job-skills';
 export * from './model/options';
@@ -7,11 +8,10 @@ export * from './model/personal-background';
 export * from './model/steps';
 export * from './model/storage';
 export * from './model/target-job-skills';
-export * from './model/validation';
-export * from './qureies';
-export { END_POINT } from './model/end_point';
 export {
   type GetMajorListResponse,
   type OnboardingForm,
   type OnboardingFormRequest,
 } from './model/types';
+export * from './model/validation';
+export * from './qureies';
