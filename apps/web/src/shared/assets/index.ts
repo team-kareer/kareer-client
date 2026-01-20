35a1fdd1f1ac295179c86f0e8eb9b2e9eb95345f
Yoonji Kim <161998554+yooncandooit@users.noreply.github.com>
2026-01-20 17:20:49 +0900

    Feat(web): 로딩 페이지 구현 (#134)
    * feat: 페이지 로더 구현
    
    * feat: layout 파일에 적용
    
    * chore: pnpm-lock 재설치
    
    * fix: 타입 정의 수정
    
    * refactor: paddingTop 수정
    
    * fix: 빌드 오류 해결

--- apps/web/src/shared/assets/index.ts
+++ apps/web/src/shared/assets/index.ts
@@ -1,6 +1,7 @@
 export { default as bg_career_roadmap } from './bg_career_roadmap.webp';
 export { default as bg_progress_bar } from './bg_progress_bar.webp';
 export { default as default_company_image } from './default_company_image.webp';
+export { default as loadingAnimation } from './lottie/loading.json';
 export { default as status_default } from './status_default.webp';
 export { default as status_graduation_countdown } from './status_graduation_countdown.webp';
 export { default as status_remaining_stay } from './status_remaining_stay.webp';
--- apps/web/src/shared/router/global-layout.tsx
+++ apps/web/src/shared/router/global-layout.tsx
@@ -5,6 +5,7 @@ import { Outlet } from 'react-router';
 
 import ErrorPage from '@pages/error/error';
 import { AppLayout } from '@widgets/layout';
+import { PageLoader } from '@shared/ui';
 
 const GlobalLayout = () => {
   return (
@@ -15,7 +16,7 @@ const GlobalLayout = () => {
             onReset={reset}
             fallback={({ resetError }) => <ErrorPage onAction={resetError} />}
           >
-            <Suspense fallback={null}>
+            <Suspense fallback={<PageLoader />}>
               <Outlet />
             </Suspense>
           </Sentry.ErrorBoundary>
