f7394ffb8bcce7e71e869cfd737213c21a0acc87
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-01-21 20:15:39 +0900

    feat: Phase type 추가

--- apps/web/src/entities/phase/model/index.ts
+++ apps/web/src/entities/phase/model/index.ts
@@ -1,2 +1,2 @@
 export { END_POINT } from './end_point';
-export type { GetPhaseListResponse } from './types';
+export type { GetPhaseListResponse, Phase } from './types';
--- apps/web/src/entities/phase/model/types.ts
+++ apps/web/src/entities/phase/model/types.ts
@@ -1,4 +1,5 @@
-import { paths } from '@shared/types/schema';
+import { components, paths } from '@shared/types/schema';
 
 export type GetPhaseListResponse =
   paths['/api/v1/phases']['get']['responses']['200']['content']['*/*'];
+export type Phase = components['schemas']['PhaseResponse'];
