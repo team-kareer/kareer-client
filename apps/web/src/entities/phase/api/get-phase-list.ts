708bceab674a5dc8f9b91899da9c593250b79858
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-01-21 18:17:16 +0900

    refactor: api response 타입 설정 방식 수정

--- apps/web/src/entities/phase/api/get-phase-list.ts
+++ apps/web/src/entities/phase/api/get-phase-list.ts
@@ -2,9 +2,9 @@ import { END_POINT, GetPhaseListResponse } from '@entities/phase/model';
 import { api } from '@shared/apis/configs/instance';
 
 export const getPhaseList = async () => {
-  const response: GetPhaseListResponse = await api
+  const response = await api
     .get(END_POINT.PHASE.GET_PHASE_LIST)
-    .json();
+    .json<GetPhaseListResponse>();
 
   return response.data;
 };
