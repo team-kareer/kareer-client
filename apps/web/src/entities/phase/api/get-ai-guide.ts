848727b5b5efda0bc0b8adc138a995730cd0c43a
jeonghoon11 <jih361@naver.com>
2026-01-21 19:18:53 +0900

    feat: fetch 코드 작성

---
+++ apps/web/src/entities/phase/api/get-ai-guide.ts
@@ -0,0 +1,11 @@
+import { END_POINT } from '@entities/phase/model/end-point';
+import { GetAIGuideResponse } from '@entities/phase/model/types';
+import { api } from '@shared/apis/configs/instance';
+
+export const getAiGuide = async (phaseActionId: number) => {
+  const response = await api
+    .get(END_POINT.PHASE.GET_AI_GUIDE(phaseActionId))
+    .json<GetAIGuideResponse>();
+
+  return response.data;
+};
