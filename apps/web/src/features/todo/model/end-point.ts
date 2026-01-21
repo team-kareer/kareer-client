4114138c7d4acaa6c607f4e1d8d0e0722eef56f0
jeonghoon11 <jih361@naver.com>
2026-01-21 15:11:44 +0900

    feat: todo toggle end-point 추가

--- apps/web/src/features/todo/model/end-point.ts
+++ apps/web/src/features/todo/model/end-point.ts
@@ -2,5 +2,7 @@ export const END_POINT = {
   TODO: {
     POST_TODO_ITEM: (phaseActionId: number) =>
       `api/v1/phase-actions/${phaseActionId}/todo`,
+    PATCH_TOGGLE_CHECKBOX: (actionItemId: number) =>
+      `pai/v1/phase-action/${actionItemId}/todo`,
   },
 };
