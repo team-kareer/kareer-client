64ae6902ed8989185774e49bd04e7ae2e7675384
jeonghoon11 <jih361@naver.com>
2026-01-22 21:56:41 +0900

    chore: 주석 제거

--- apps/web/src/widgets/roadmap/ui/accordion-list/accordion-list.tsx
+++ apps/web/src/widgets/roadmap/ui/accordion-list/accordion-list.tsx
@@ -7,7 +7,6 @@ import { PHASE_QUERY_OPTIONS } from '@entities/phase/queries';
 import * as styles from './accordion-list.css';
 
 const AccordionList = () => {
-  // 추후 api 응답 값으로 변경
   const { data } = useQuery({ ...PHASE_QUERY_OPTIONS.GET_PHASE_LIST() });
   return (
     <section className={styles.container}>
