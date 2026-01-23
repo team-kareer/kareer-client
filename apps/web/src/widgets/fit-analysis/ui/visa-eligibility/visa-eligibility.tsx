033df02183a4f3846bd060f709e63096c9c0727b
JeongHoon <113702672+jeonghoon11@users.noreply.github.com>
2026-01-23 16:09:40 +0900

    feat: 핏 분석 api 연동 (#200)

--- apps/web/src/widgets/fit-analysis/ui/visa-eligibility/visa-eligibility.tsx
+++ apps/web/src/widgets/fit-analysis/ui/visa-eligibility/visa-eligibility.tsx
@@ -1,4 +1,5 @@
 import { Tab } from '@kds/ui';
+import { useQuery } from '@tanstack/react-query';
 
 import {
   VISA_CHECKLIST_D2,
@@ -8,6 +9,8 @@ import {
   VisaChecklist,
   VisaTabList,
 } from '@widgets/fit-analysis';
+import { USER_QUERY_OPTIONS } from '@entities/user/queries';
+import { formatDate } from '@shared/utils/date-formatter';
 
 import * as styles from './visa-eligibility.css';
 
@@ -18,21 +21,37 @@ const VISA_TAB_PANELS = [
 ] as const;
 
 const VisaEligibility = () => {
-  const mockData = {
-    visa: 'D-2 Student',
-    date: 'Dec 10. 2026',
+  const { data } = useQuery({ ...USER_QUERY_OPTIONS.GET_USER_STATUS() });
+
+  let currentVisa: (typeof VISA_LIST)[keyof typeof VISA_LIST] = VISA_LIST.D2;
+  let rawDate = data?.visaExpiredAt;
+
+  if (data?.visaType === 'D2') {
+    currentVisa = VISA_LIST.D2;
+    rawDate = data?.visaExpiredAt;
+  } else if (data?.visaType === 'D10') {
+    currentVisa = VISA_LIST.D10;
+    rawDate = data?.visaExpiredAt;
+  } else if (data?.visaType === 'E7') {
+    currentVisa = VISA_LIST.E7;
+    rawDate = undefined;
+  }
+
+  const formatData = {
+    visa: currentVisa,
+    date: formatDate(rawDate),
   };
   return (
-    <Tab.Container initialValue={mockData.visa} className={styles.container}>
+    <Tab.Container initialValue={formatData.visa} className={styles.container}>
       <Tab.List className={styles.tabContainer}>
-        <VisaTabList visa={mockData.visa} />
+        <VisaTabList visa={formatData.visa} />
       </Tab.List>
       {VISA_TAB_PANELS.map(({ tab, checklistData }) => (
         <Tab.Panel key={tab} className={styles.panelContainer} tab={tab}>
           <VisaChecklist
-            isCurrentVisa={mockData.visa === tab}
+            isCurrentVisa={formatData.visa === tab}
             curTab={tab}
-            date={mockData.date}
+            date={formatData.date}
             checklistData={checklistData}
           />
         </Tab.Panel>
