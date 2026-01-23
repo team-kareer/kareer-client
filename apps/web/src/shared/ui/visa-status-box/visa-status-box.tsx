7bb86b00a4aed318af6ba84307f35911f3015e5b
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-01-23 22:12:39 +0900

    Fix(web): 전체적인 transition 적용 QA-2 (#212)
    * style: transition 적용
    
    * fix: replace 구문 제거
    
    * style: transition 적용

--- apps/web/src/shared/ui/visa-status-box/visa-status-box.tsx
+++ apps/web/src/shared/ui/visa-status-box/visa-status-box.tsx
@@ -18,7 +18,6 @@ const VisaStatusBox = ({
   total,
   done,
 }: VisaStatusBoxProps) => {
-  const formattedDate = date.replace(', ', '. ');
   const percent = Math.floor((done / total) * 100);
   const completed = percent === 100;
   const color = completed ? 'pastel_mint' : 'pastel_orange';
@@ -35,7 +34,7 @@ const VisaStatusBox = ({
             </>
           )}
           <span className={styles.current}>{goal}</span>
-          {isCurrent && <span className={styles.date}>{formattedDate}</span>}
+          {isCurrent && <span className={styles.date}>{date}</span>}
         </div>
         <div className={styles.right_section}>
           <Tag color={color}>{tagText}</Tag>
