e4efd46f3a05b61ff21095424021f0ccf52baa39
jeonghoon11 <jih361@naver.com>
2026-02-22 13:56:18 +0900

    feat: GA4 초기화 연결

--- apps/web/src/app/main.tsx
+++ apps/web/src/app/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { InitAnalytics } from '@shared/configs/analytics';
 import InitSentry from '@shared/configs/sentry';
 
 import App from './App';
 
 InitSentry();
+InitAnalytics();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
