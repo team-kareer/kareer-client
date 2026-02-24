c9d8869e30eed375db7c9d1b66a9917f6990f9db
jeonghoon11 <jih361@naver.com>
2026-02-25 03:14:39 +0900

    feat: todo 테스트용 다국어 추가

--- apps/web/src/shared/i18n/i18n.ts
+++ apps/web/src/shared/i18n/i18n.ts
@@ -2,16 +2,20 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import enCommon from '@shared/i18n/locales/en/common.json';
+import enTodo from '@shared/i18n/locales/en/todo.json';
 import koCommon from '@shared/i18n/locales/ko/common.json';
+import koTodo from '@shared/i18n/locales/ko/todo.json';
 import viCommon from '@shared/i18n/locales/vi/common.json';
+import viTodo from '@shared/i18n/locales/vi/todo.json';
 import zhCnCommon from '@shared/i18n/locales/zh-CN/common.json';
+import zhCnTodo from '@shared/i18n/locales/zh-CN/todo.json';
 
 i18n.use(initReactI18next).init({
   resources: {
-    en: { common: enCommon },
-    ko: { common: koCommon },
-    vi: { common: viCommon },
-    'zh-CN': { common: zhCnCommon },
+    en: { common: enCommon, todo: enTodo },
+    ko: { common: koCommon, todo: koTodo },
+    vi: { common: viCommon, todo: viTodo },
+    'zh-CN': { common: zhCnCommon, todo: zhCnTodo },
   },
   lng: 'en',
   fallbackLng: 'en',
