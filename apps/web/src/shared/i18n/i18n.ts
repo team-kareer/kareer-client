e0bf9184f0f40b8eed125a24c3c447c2e8ed8269
jeonghoon11 <jih361@naver.com>
2026-02-25 03:16:44 +0900

    feat: resources 파일 분리

--- apps/web/src/shared/i18n/i18n.ts
+++ apps/web/src/shared/i18n/i18n.ts
@@ -1,26 +1,14 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import enCommon from '@shared/i18n/locales/en/common.json';
-import enTodo from '@shared/i18n/locales/en/todo.json';
-import koCommon from '@shared/i18n/locales/ko/common.json';
-import koTodo from '@shared/i18n/locales/ko/todo.json';
-import viCommon from '@shared/i18n/locales/vi/common.json';
-import viTodo from '@shared/i18n/locales/vi/todo.json';
-import zhCnCommon from '@shared/i18n/locales/zh-CN/common.json';
-import zhCnTodo from '@shared/i18n/locales/zh-CN/todo.json';
+import { resources } from '@shared/i18n/resources';
 
 i18n.use(initReactI18next).init({
-  resources: {
-    en: { common: enCommon, todo: enTodo },
-    ko: { common: koCommon, todo: koTodo },
-    vi: { common: viCommon, todo: viTodo },
-    'zh-CN': { common: zhCnCommon, todo: zhCnTodo },
-  },
+  resources,
   lng: 'en',
   fallbackLng: 'en',
   supportedLngs: ['en', 'ko', 'vi', 'zh-CN'],
-  ns: ['common'],
+  ns: ['common, todo'],
   defaultNS: 'common',
   interpolation: {
     escapeValue: false,
