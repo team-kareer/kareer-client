2241c0630fe10d70c7028bebad819c22d71425dc
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-03-13 22:05:38 +0900

    style: 팝오버 애니메이션 구현

--- apps/web/src/shared/ui/popover/popover.css.ts
+++ apps/web/src/shared/ui/popover/popover.css.ts
@@ -10,6 +10,26 @@ export const base = style({
   borderRadius: '20px',
   boxShadow: '0 0 12px 0 rgba(0, 0, 0, 0.25)',
   positionAnchor: anchor,
+
+  opacity: 0,
+  transform: 'scale(0)',
+  transition:
+    'opacity 0.3s ease, transform 0.3s ease, display 0.3s allow-discrete, overlay 0.3s allow-discrete',
+
+  selectors: {
+    '&:popover-open': {
+      opacity: 1,
+      transform: 'scale(1)',
+    },
+  },
+  '@starting-style': {
+    selectors: {
+      '&:popover-open': {
+        opacity: 0,
+        transform: 'scale(0)',
+      },
+    },
+  },
 });
 
 export const placement = styleVariants({
@@ -17,20 +37,24 @@ export const placement = styleVariants({
     bottom: `calc(anchor(bottom) + ${offsetY})`,
     left: 'anchor(left)',
     right: 'auto',
+    transformOrigin: 'bottom left',
   },
   'top-end': {
     bottom: `calc(anchor(bottom) + ${offsetY})`,
     left: 'auto',
     right: 'anchor(right)',
+    transformOrigin: 'bottom right',
   },
   'bottom-start': {
     top: `calc(anchor(bottom) + ${offsetY})`,
     left: 'anchor(left)',
     right: 'auto',
+    transformOrigin: 'top left',
   },
   'bottom-end': {
     top: `calc(anchor(bottom) + ${offsetY})`,
     left: 'auto',
     right: 'anchor(right)',
+    transformOrigin: 'top right',
   },
 });
