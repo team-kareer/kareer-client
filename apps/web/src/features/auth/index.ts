aada5274c242687d600476c546bf00dd11930356
huniversal <hunivers0523@naver.com>
2026-03-15 19:13:13 +0900

    feat: 테스트 버튼 제거

--- apps/web/src/features/auth/index.ts
+++ apps/web/src/features/auth/index.ts
@@ -1,4 +1,2 @@
 export { default as LoginButton } from './ui/login-button/login-button';
 export { default as LoginCallback } from './ui/login-callback/login-callback';
-// TODO: Test 버튼
-export { default as TestLogoutButton } from './ui/test-logout-button/test-logout-button';
