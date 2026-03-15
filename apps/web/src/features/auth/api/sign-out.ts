4440a83664e13cae8b93b1df8aa4e1eb648e752f
huniversal <hunivers0523@naver.com>
2026-03-15 23:58:13 +0900

    fix: 절대경로 수정

--- apps/web/src/features/auth/api/sign-out.ts
+++ apps/web/src/features/auth/api/sign-out.ts
@@ -1,7 +1,6 @@
 import { END_POINT } from '@features/auth/model/end_point';
+import { LogOutResponse } from '@features/auth/model/types';
 import { api } from '@shared/apis/configs/instance';
-
-import { LogOutResponse } from '../model/types';
 
 export const logOut = async (): Promise<LogOutResponse> => {
   return api.post(END_POINT.LOGOUT.SIGN_OUT).json<LogOutResponse>();
