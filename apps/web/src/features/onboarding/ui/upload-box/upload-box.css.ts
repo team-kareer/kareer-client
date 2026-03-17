f82967dd3827abd7f4400126f4a29ff0e4d8c627
jeonghoon11 <jih361@naver.com>
2026-03-17 14:58:54 +0900

    feat: upload-box 컴포넌트 구현

---
+++ apps/web/src/features/onboarding/ui/upload-box/upload-box.css.ts
@@ -0,0 +1,76 @@
+import { themeVars, typography } from '@kds/ui/styles';
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1rem',
+  alignItems: 'center',
+  width: '33rem',
+  // height: '26.6rem',
+  padding: '1.2rem',
+
+  border: `1px solid ${themeVars.color.grayscale.gray300}`,
+  borderRadius: '20px',
+  backgroundColor: themeVars.color.grayscale.white,
+});
+
+export const uploadContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '1.2rem',
+  width: '100%',
+  height: '100%',
+  padding: '2.8rem 9.4rem',
+
+  border: `1px dashed ${themeVars.color.grayscale.gray400}`,
+  borderRadius: '10px',
+});
+
+export const uploadTopSection = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '0.4rem',
+});
+
+export const text = style({
+  ...typography.body8_m_14,
+  color: themeVars.color.grayscale.gray500,
+});
+
+export const fileContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1rem',
+  width: '100%',
+  padding: '1.4rem 1.8rem',
+
+  border: `1px solid ${themeVars.color.grayscale.gray300}`,
+  borderRadius: '10px',
+});
+
+export const fileTopSection = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
+
+export const fileSection = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.4rem',
+});
+
+export const fileName = style({
+  ...typography.body7_sb_14,
+  color: themeVars.color.grayscale.gray700,
+});
+
+export const fileSize = style({
+  ...typography.cap2_m_12,
+  color: themeVars.color.grayscale.gray400,
+});
---
+++ apps/web/src/features/onboarding/ui/upload-box/upload-box.tsx
@@ -0,0 +1,62 @@
+import { UploadIcon, XIcon } from '@kds/icons';
+import { Button, ProgressBar } from '@kds/ui';
+
+import * as styles from './upload-box.css';
+
+interface UploadBoxProps {
+  file?: File;
+  progress?: {
+    done: number;
+    total: number;
+  };
+  onChooseFile: () => void;
+  onRemoveFile: () => void;
+}
+
+const formatFileSize = (fileSize: number) => {
+  const kilobytes = fileSize / 1024;
+
+  if (kilobytes < 1024) {
+    return `${Math.round(kilobytes)}KB`;
+  }
+
+  return `${(kilobytes / 1024).toFixed(1)}MB`;
+};
+
+const UploadBox = ({
+  file,
+  progress,
+  onChooseFile,
+  onRemoveFile,
+}: UploadBoxProps) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.uploadContainer}>
+        <div className={styles.uploadTopSection}>
+          <UploadIcon width="2.4rem" height="2.4rem" />
+          <p className={styles.text}>Upload Photo</p>
+        </div>
+        <Button preset="medium_secondary" onClick={onChooseFile}>
+          Choose File
+        </Button>
+      </div>
+
+      {file && (
+        <div className={styles.fileContainer}>
+          <div className={styles.fileTopSection}>
+            <div className={styles.fileSection}>
+              <p className={styles.fileName}>{file.name}</p>
+              <p className={styles.fileSize}>{formatFileSize(file.size)}</p>
+            </div>
+            <XIcon width="1.6rem" height="1.6rem" onClick={onRemoveFile} />
+          </div>
+          {progress && (
+            <ProgressBar total={progress.total} done={progress.done} />
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UploadBox;
