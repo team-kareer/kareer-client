86ec914ec4082e929a37cd8ca3f2461025482c94
huniversal <hunivers0523@naver.com>
2026-03-19 02:26:46 +0900

    feat: GetUserMyPageResponse 타입 추가

--- apps/web/src/entities/user/model/types.ts
+++ apps/web/src/entities/user/model/types.ts
@@ -4,3 +4,6 @@ export type GetUserInfoResponse =
   paths['/api/v1/members/me']['get']['responses']['200']['content']['*/*'];
 export type GetUserStatusResponse =
   paths['/api/v1/members/me/status']['get']['responses']['200']['content']['*/*'];
+
+export type GetUserMyPageResponse =
+  paths['/api/v1/members/mypage']['get']['responses']['200']['content']['*/*'];
