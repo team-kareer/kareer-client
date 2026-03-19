f68f56ed74e59486d37acf0162d7a7cf19a79873
huniversal <hunivers0523@naver.com>
2026-03-19 20:59:36 +0900

    refactor: constants 분리

--- apps/web/src/widgets/my-page/ui/profile-info/profile-info.tsx
+++ apps/web/src/widgets/my-page/ui/profile-info/profile-info.tsx
@@ -1,32 +1,33 @@
 import { useQuery } from '@tanstack/react-query';
 
-import { ProfileField } from '@entities/user/ui';
+import { PROFILE_FIELD_SECTIONS } from '@widgets/my-page/constants/profile-field-sections';
 import { USER_QUERY_OPTIONS } from '@entities/user/queries';
+import { ProfileField } from '@entities/user/ui';
 import { PageLoader } from '@shared/ui';
 
 import * as styles from './profile-info.css';
 
-const PROFILE_FIELD_SECTIONS = [
-  [{ label: 'Target Job', key: 'targetJob' }],
-  [
-    { label: 'Date of Birth', key: 'birthDate' },
-    { label: 'Country', key: 'country' },
-  ],
-  [
-    { label: 'Degree', key: 'degree' },
-    { label: 'University', key: 'university' },
-    { label: 'Major', key: 'primaryMajor' },
-    { label: 'Secondary Major', key: 'secondaryMajor' },
-  ],
-  [
-    { label: 'Current Visa Type', key: 'visaType' },
-    { label: 'Current Visa Start Date', key: 'visaExpiredAt' },
-  ],
-  [
-    { label: 'TOPIK / KIIP Level', key: 'languageLevel' },
-    { label: 'English Level', key: 'englishLevel' },
-  ],
-] as const;
+// const PROFILE_FIELD_SECTIONS = [
+//   [{ label: 'Target Job', key: 'targetJob' }],
+//   [
+//     { label: 'Date of Birth', key: 'birthDate' },
+//     { label: 'Country', key: 'country' },
+//   ],
+//   [
+//     { label: 'Degree', key: 'degree' },
+//     { label: 'University', key: 'university' },
+//     { label: 'Major', key: 'primaryMajor' },
+//     { label: 'Secondary Major', key: 'secondaryMajor' },
+//   ],
+//   [
+//     { label: 'Current Visa Type', key: 'visaType' },
+//     { label: 'Current Visa Start Date', key: 'visaExpiredAt' },
+//   ],
+//   [
+//     { label: 'TOPIK / KIIP Level', key: 'languageLevel' },
+//     { label: 'English Level', key: 'englishLevel' },
+//   ],
+// ] as const;
 
 const ProfileInfo = () => {
   const { data, isPending } = useQuery({ ...USER_QUERY_OPTIONS.GET_MY_PAGE() });
