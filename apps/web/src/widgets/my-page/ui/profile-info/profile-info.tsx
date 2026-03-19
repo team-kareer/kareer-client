65ac73acc9750135de8409e3bac21a35db955fb6
huniversal <hunivers0523@naver.com>
2026-03-19 19:11:23 +0900

    feat: 마이페이지 api 연동

--- apps/web/src/widgets/my-page/ui/profile-info/profile-info.tsx
+++ apps/web/src/widgets/my-page/ui/profile-info/profile-info.tsx
@@ -1,4 +1,7 @@
+import { useQuery } from '@tanstack/react-query';
+
 import { ProfileField } from '@widgets/my-page';
+import { USER_QUERY_OPTIONS } from '@entities/user/queries';
 
 import * as styles from './profile-info.css';
 
@@ -25,12 +28,13 @@ const PROFILE_FIELD_SECTIONS = [
 ] as const;
 
 const ProfileInfo = () => {
+  const { data } = useQuery({ ...USER_QUERY_OPTIONS.GET_MY_PAGE() });
   return (
     <section className={styles.container}>
       {PROFILE_FIELD_SECTIONS.map((section, index) => (
         <div key={index} className={styles.section}>
           {section.map(({ label, key }) => (
-            <ProfileField key={key} label={label} value="훈진" />
+            <ProfileField key={key} label={label} value={data?.[key] ?? ''} />
           ))}
         </div>
       ))}
