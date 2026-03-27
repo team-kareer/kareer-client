e96d7e1adfb0f8b436933aff92da4a089b8e63a8
jeonghoon11 <jih361@naver.com>
2026-03-27 23:07:42 +0900

    feat: 내비게이션 다국어 설정

--- apps/web/src/shared/i18n/resources.ts
+++ apps/web/src/shared/i18n/resources.ts
@@ -1,15 +1,23 @@
 import enCommon from '@shared/i18n/locales/en/common.json';
+import enNavigation from '@shared/i18n/locales/en/navigation.json';
 import enTodo from '@shared/i18n/locales/en/todo.json';
 import koCommon from '@shared/i18n/locales/ko/common.json';
+import koNavigation from '@shared/i18n/locales/ko/navigation.json';
 import koTodo from '@shared/i18n/locales/ko/todo.json';
 import viCommon from '@shared/i18n/locales/vi/common.json';
+import viNavigation from '@shared/i18n/locales/vi/navigation.json';
 import viTodo from '@shared/i18n/locales/vi/todo.json';
 import zhCnCommon from '@shared/i18n/locales/zh-CN/common.json';
+import zhCnNavigation from '@shared/i18n/locales/zh-CN/navigation.json';
 import zhCnTodo from '@shared/i18n/locales/zh-CN/todo.json';
 
 export const resources = {
-  en: { common: enCommon, todo: enTodo },
-  ko: { common: koCommon, todo: koTodo },
-  vi: { common: viCommon, todo: viTodo },
-  'zh-CN': { common: zhCnCommon, todo: zhCnTodo },
+  en: { common: enCommon, navigation: enNavigation, todo: enTodo },
+  ko: { common: koCommon, navigation: koNavigation, todo: koTodo },
+  vi: { common: viCommon, navigation: viNavigation, todo: viTodo },
+  'zh-CN': {
+    common: zhCnCommon,
+    navigation: zhCnNavigation,
+    todo: zhCnTodo,
+  },
 } as const;
