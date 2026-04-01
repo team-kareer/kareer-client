f44b81e30cceaee80ed1be369fbd29bfccd2bcd1
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-04-01 20:13:57 +0900

    feat: passport, visa ocr api 연결

--- apps/web/src/widgets/onboarding/ui/step/identity-visaVerification/identity-visaVerification.tsx
+++ apps/web/src/widgets/onboarding/ui/step/identity-visaVerification/identity-visaVerification.tsx
@@ -1,10 +1,17 @@
 import { useState } from 'react';
+import { useMutation } from '@tanstack/react-query';
+import { useFormContext } from 'react-hook-form';
 
 import { OnboardingStepTitle } from '@widgets/onboarding';
 import ImageUploadSection from '@widgets/onboarding/ui/step/identity-visaVerification/ui/image-upload-section/image-upload-section';
 import UserInfoFormSection from '@widgets/onboarding/ui/step/identity-visaVerification/ui/user-info-form-section/user-info-form-section';
 import VisaInfoFormSection from '@widgets/onboarding/ui/step/identity-visaVerification/ui/visa-info-form-section/visa-info-form-section';
-import { FUNNEL_STEPS } from '@entities/onboarding';
+import {
+  ONBOARDING_MUTATION_OPTIONS,
+  UploadProgress,
+  VisaType,
+} from '@features/onboarding';
+import { FUNNEL_STEPS, OnboardingForm } from '@entities/onboarding';
 
 import * as styles from './identity-visaVerification.css';
 
@@ -16,7 +23,35 @@ type UploadState = {
   errorMessage?: string;
 };
 
+const VISA_TYPE_MAP: Record<string, VisaType | ''> = {
+  D2: 'D-2',
+  D10: 'D-10',
+} as const;
+
 const IdentityVisaVerification = () => {
+  const { setValue } = useFormContext<OnboardingForm>();
+
+  const transformVisaType = (serverValue: string): VisaType | '' => {
+    return (serverValue && VISA_TYPE_MAP[serverValue]) || '';
+  };
+
+  const { mutate: submitPassportFile } = useMutation({
+    ...ONBOARDING_MUTATION_OPTIONS.POST_OCR_PASSPORT(),
+    onSuccess: (data) => {
+      setValue('name', data.data?.fullName ?? '');
+      setValue('countryCode', data.data?.country ?? '');
+      setValue('birthDate', data.data?.birthDate ?? '');
+    },
+  });
+  const { mutate: submitVisaFile } = useMutation({
+    ...ONBOARDING_MUTATION_OPTIONS.POST_OCR_VISA(),
+    onSuccess: (data) => {
+      setValue('visaType', transformVisaType(data.data?.visaType ?? ''));
+      setValue('visaStartDate', data.data?.visaStartDate ?? '');
+      setValue('visaExpiredAt', data.data?.visaExpiredAt ?? '');
+    },
+  });
+
   const [passportUpload, setPassportUpload] = useState<UploadState>({});
   const [visaUpload, setVisaUpload] = useState<UploadState>({});
 
@@ -34,6 +69,32 @@ const IdentityVisaVerification = () => {
     };
   };
 
+  const [visaProgress, setVisaProgress] = useState<UploadProgress>();
+
+  const handleSelectVisa = (file: File) => {
+    const state = getUploadState(file);
+    setVisaUpload(state);
+    if (!state.file) {
+      return;
+    }
+
+    submitVisaFile({
+      file,
+      onProgress: (progress) => {
+        setVisaProgress(progress);
+      },
+    });
+  };
+
+  const handleSelectPassport = (file: File) => {
+    const state = getUploadState(file);
+    setPassportUpload(state);
+    if (!state.file) {
+      return;
+    }
+    submitPassportFile(file);
+  };
+
   return (
     <section className={styles.container}>
       <OnboardingStepTitle stepNumber={1} title={FUNNEL_STEPS[0]} />
@@ -44,7 +105,7 @@ const IdentityVisaVerification = () => {
             subtitle="Up to 1 files · 10 MB max"
             file={passportUpload.file}
             errorMessage={passportUpload.errorMessage}
-            onSelectFile={(file) => setPassportUpload(getUploadState(file))}
+            onSelectFile={handleSelectPassport}
             onRemoveFile={() => setPassportUpload({})}
           />
           <UserInfoFormSection />
@@ -57,8 +118,9 @@ const IdentityVisaVerification = () => {
             title="Upload Visa or ARC"
             subtitle="Up to 1 files · 10 MB max"
             file={visaUpload.file}
+            progress={visaProgress}
             errorMessage={visaUpload.errorMessage}
-            onSelectFile={(file) => setVisaUpload(getUploadState(file))}
+            onSelectFile={handleSelectVisa}
             onRemoveFile={() => setVisaUpload({})}
           />
           <VisaInfoFormSection />
