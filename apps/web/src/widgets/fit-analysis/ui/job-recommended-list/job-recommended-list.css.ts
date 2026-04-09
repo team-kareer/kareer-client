ba7a6f119289988eab7063d67ff0da9f5e533d8f
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-04-10 01:21:19 +0900

    style: 로딩 영역 배경 추가

--- apps/web/src/widgets/fit-analysis/ui/job-recommended-list/job-recommended-list.css.ts
+++ apps/web/src/widgets/fit-analysis/ui/job-recommended-list/job-recommended-list.css.ts
@@ -1,7 +1,14 @@
+import { themeVars } from '@kds/ui/styles';
 import { style } from '@vanilla-extract/css';
 
 export const listWrapper = style({
   display: 'flex',
   flexDirection: 'column',
   gap: '1.6rem',
+});
+
+export const loaderWrapper = style({
+  height: '30.2rem',
+  borderRadius: '20px',
+  backgroundColor: themeVars.color.grayscale.gray200,
 });
