78771fb0568899ed6229974c6796cffe8828bffc
huniversal <hunivers0523@naver.com>
2026-08-23 16:31:17 +0900

    feat: 랜딩 페이지 다국어 구조 세팅

---
+++ apps/landing/src/i18n/resources.ts
@@ -0,0 +1,11 @@
+import enLanding from '@i18n/locales/en/landing.json';
+import koLanding from '@i18n/locales/ko/landing.json';
+
+export const resources = {
+  en: {
+    landing: enLanding,
+  },
+  ko: {
+    landing: koLanding,
+  },
+} as const;
